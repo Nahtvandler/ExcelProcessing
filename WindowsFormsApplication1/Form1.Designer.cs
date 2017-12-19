920979443a426b3011a51c5c138fee164c3cc690
Михаил <9misha97@mail.ru>
2017-12-19 15:13:13 +0300

    dev

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(502, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "проверить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 480);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "3";
+            this.label1.Visible = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(461, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(26, 20);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(340, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Путь к xlsl:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Путь к папке:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(115, 66);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(340, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(461, 66);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(26, 20);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(169, 348);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(66, 30);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "проверено:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(166, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "0 из ...";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(12, 146);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(128, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "прошло проверку:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(166, 147);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 17);
+            this.label7.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(12, 173);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(148, 17);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "не прошло проверку:";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(166, 174);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 17);
+            this.label9.TabIndex = 14;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 217);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(261, 26);
+            this.progressBar1.TabIndex = 16;
+            this.progressBar1.Visible = false;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(595, 31);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(476, 481);
+            this.richTextBox1.TabIndex = 17;
+            this.richTextBox1.Text = "";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(543, 516);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(34, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "v. 0.2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1109, 538);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public static string XPath, FPath,fcount;
+        //public static int fcount;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            XPath = null;
+            FPath = null;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            //string[] list = new string[LastCell.Row];
+            if (XPath == null || FPath == null)
+            {
+                MessageBox.Show("Не выбран путь для xlsl файла или директории с файлами");
+                return;
+            }
+
+            //открытие excel файла и формирования массива занчений первой ячейки
+            Excel.Application ObjWorkExcel = new Excel.Application();
+            Excel.Workbook ObjWorkbook = ObjWorkExcel.Workbooks.Open(XPath, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            Excel.Worksheet ObjWorkSheet = (Excel.Worksheet)ObjWorkbook.Sheets[1];
+            var LastCell = ObjWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);
+            //
+            int LastColumn = (int)LastCell.Column;
+            int LastRow = (int)LastCell.Row;
+            //
+            string[] list = new string[LastCell.Row];
+            string efname;
+            label5.Text = "0 из " + (int)LastCell.Row; //кол-во строк
+            //progressBar1.Maximum = (int)LastCell.Row; //
+            //progressBar1.Minimum = 1; //
+            for (int i = 0; i < (int)LastCell.Row; i++)
+            {
+                //собираем имя файла из ячеек
+                efname = ObjWorkSheet.Cells[i + 1, 1].text.ToString()+"_"+ ObjWorkSheet.Cells[i + 1, 2].text.ToString()+"_"+
+                    ObjWorkSheet.Cells[i + 1, 3].text.ToString()+"_"+ObjWorkSheet.Cells[i + 1, 4].text.ToString().Replace("/", "_I_");
+                    //efname = efname.Replace("_I_", "/");
+                list[i] = efname;
+                //richTextBox1.Text = "Собранное имя: " + richTextBox1.Text + Environment.NewLine + list[i];
+                //-собираем имя файла из ячеек
+
+                //list[i] = ObjWorkSheet.Cells[i + 1, 4].text.ToString();
+                //progressBar1.Value = i+1;
+                //label1.Text = i.ToString(); //
+            }
+            
+            //ObjWorkbook.Close(false, Type.Missing, Type.Missing);
+            //ObjWorkExcel.Quit();
+            //GC.Collect();
+           
+            //получение имен файлов
+            string nstr,fname;
+            int count=0,acount=0,flcount=0;
+            Boolean chek=false;
+            DirectoryInfo dirInfo = new DirectoryInfo(FPath);
+            FileInfo[] files = dirInfo.GetFiles();
+            for (int j = 0; j < LastRow; j++)
+            //foreach (FileInfo file in files)
+            {
+                count = count + 1;
+                //textBox3.Text = textBox3.Text + Environment.NewLine + "проверяем файл: "+file.Name; //
+                foreach (FileInfo file in files)
+                //for (int j = 0; j < LastRow; j++)
+                {
+                    nstr = file.Name;
+                    //richTextBox1.Text = richTextBox1.Text + Environment.NewLine + "имя файла: " + nstr;
+                    fname = nstr;
+                    //nstr = nstr.Substring(nstr.IndexOf("_N_") + 3, nstr.IndexOf(".") - (nstr.IndexOf("_N_") + 3));
+                    nstr = nstr.Substring(0, nstr.LastIndexOf("."));
+                    //richTextBox1.Text = richTextBox1.Text + Environment.NewLine + "Проверяемое имя: " + nstr;
+                    label1.Text = nstr;
+                    if (list[j] == nstr)
+                    {
+
+                        //добавление ссылки
+                        Excel.Range rangeToHoldHyperlink = (Excel.Range)ObjWorkSheet.Cells[j+1, 15];
+
+                        string hyperlinkTargetAddress = file.FullName;
+                        ObjWorkSheet.Hyperlinks.Add(rangeToHoldHyperlink,hyperlinkTargetAddress,"", "Screen Tip Text", hyperlinkTargetAddress);
+                        //
+
+                        //счетчик файлов которые успешно прошли проверку
+                        acount = acount + 1;
+                        label7.Text = acount.ToString();
+                        //textBox3.Text = textBox3.Text + Environment.NewLine + "прошел проверку"+fname; //
+                        chek = true;
+                        break;
+                    }
+                    else
+                    {
+                        //счетчик + к файлам которых нет, добавить в текстбокс
+                        
+                        //label9.Text =flcount.ToString();
+                        //textBox3.Text = textBox3.Text + Environment.NewLine + "не прошел проверку" + fname; //
+                        //textBox3.Text = textBox3.Text + Environment.NewLine + fname;
+                        chek = false;
+                    }
+                }
+                if (chek == false)
+                {
+                    //textBox3.Text = textBox3.Text + Environment.NewLine + file.Name;
+                    //textBox3.Text = textBox3.Text + Environment.NewLine + list[j];
+                    richTextBox1.Text = richTextBox1.Text + Environment.NewLine + list[j];
+                    flcount = flcount + 1;                    
+                }
+                label5.Text = count + " из " + LastRow;
+            }
+            label9.Text = flcount.ToString();
+
+            //сохраняем результат
+            //ObjWorkbook = ObjWorkExcel.Workbooks;
+            //excelappworkbook = excelappworkbooks[1];
+
+            //---
+            //Excel.Range excelcells;
+            //excelcells = (Excel.Range)ObjWorkSheet.Cells[1, 1];
+            //excelcells.Value2 = "test";
+
+            //excelcells = ObjWorkSheet.get_Range("A1", Type.Missing);
+            //Выводим значение текстовую строку
+            //excelcells.Value2 = "Лист 2";
+            //excelcells.Font.Size = 20;
+            //excelcells.Font.Italic = true;
+            //excelcells.Font.Bold = true;
+
+            ObjWorkExcel.DisplayAlerts = true;
+            /*
+            ObjWorkbook.SaveAs(Type.Missing,  //object Filename
+               Type.Missing,                       //object FileFormat
+               Type.Missing,                       //object Password 
+               Type.Missing,                       //object WriteResPassword  
+               Type.Missing,                       //object ReadOnlyRecommended
+               Type.Missing,                       //object CreateBackup
+               Excel.XlSaveAsAccessMode.xlNoChange,//XlSaveAsAccessMode AccessMode
+               Type.Missing,                       //object ConflictResolution
+               Type.Missing,                       //object AddToMru 
+               Type.Missing,                       //object TextCodepage
+               Type.Missing,                       //object TextVisualLayout
+               Type.Missing);                      //object Local
+               */
+            ObjWorkbook.Save(); //делаем обычный сейв вместо "сохранить как"
+
+            //ObjWorkbook.Close(false, Type.Missing, Type.Missing);
+            ObjWorkExcel.Quit();
+            GC.Collect();
+            //
+            MessageBox.Show("Документ сохранен");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = dialog.SelectedPath;
+                FPath= dialog.SelectedPath;
+
+                DirectoryInfo dirInfo = new DirectoryInfo(FPath);
+                fcount = dirInfo.GetFiles().Length.ToString();
+                //label5.Text = "0 из " + dirInfo.GetFiles().Length.ToString();
+                //label5.Text = "0 из " + fcount;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //string str = "abc_N_123_I_456_I_789.abc.text",str1="123/456/789";
+            string str = "акт_ооо ромашка_15.06.2017_85, сч.ф 8653_I_15.txt", str1 = "123/456/789";
+            
+            label1.Text = str;
+            int len = str.IndexOf(".") - str.IndexOf("_№_");
+            //str = str.Substring(str.IndexOf("_№_")+3, str.IndexOf(".")-(str.IndexOf("_№_")+3));
+            //str = str.Substring(str.IndexOf("_№_") + 3, str.LastIndexOf(".") - (str.IndexOf("_№_") + 3));
+            str = str.Replace("_I_", "/");
+            str= str.Substring(0, str.LastIndexOf(".")); ;
+            label1.Text = label1.Text+" ; " + str+" ; "+(str==str1).ToString();
+            /*
+            for (int i = 0; i < 10; i++)
+            {
+                //label1.Text = (int)label1.Text + 1;
+                textBox3.Text=textBox3.Text + Environment.NewLine + "dfgdfg";
+            }
+            
+            //label1.Text = FPath + " ; " + XPath;
+            //создание дохренища файлов-
+            Excel.Application ObjWorkExcel = new Excel.Application();
+            Excel.Workbook ObjWorkbook = ObjWorkExcel.Workbooks.Open(XPath, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            Excel.Worksheet ObjWorkSheet = (Excel.Worksheet)ObjWorkbook.Sheets[1];
+            var LastCell = ObjWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);
+            //
+            int LastColumn = (int)LastCell.Column;
+            int LastRow = (int)LastCell.Row;
+            //
+            string[] list = new string[LastCell.Row];
+            label5.Text = "0 из " + (int)LastCell.Row; //кол-во строк
+            Excel.Range excelcells;
+            //excelcells = (Excel.Range)ObjWorkSheet.Cells[1, 1];
+            //excelcells.Value2 = "test";
+
+           
+            //Выводим значение текстовую строку
+            //excelcells.Value2 = "Лист 2";
+            //excelcells.Font.Size = 20;
+            //excelcells.Font.Italic = true;
+            //excelcells.Font.Bold = true;
+
+
+            /*string path,cell;
+            int ind;
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                ind = i + 1;
+                cell = "A" + ind;
+                textBox3.Text = textBox3.Text + "A"+ind + Environment.NewLine;
+                excelcells = ObjWorkSheet.get_Range(cell, Type.Missing);
+                path = @"C:\Users\Михаил\Desktop\тестовые файлы\2\abc_N_"+rnd.Next(1000)+"_I_"+ rnd.Next(1000)+"_I_"+rnd.Next(1000)+".txt";
+                System.IO.File.Create(path);
+
+                path = path.Substring(path.IndexOf("_N_") + 3, path.IndexOf(".") - (path.IndexOf("_N_") + 3));
+                path = path.Replace("_I_", "/");
+                excelcells.Value2 = path;
+
+            }
+            ObjWorkExcel.DisplayAlerts = true;
+            ObjWorkbook.Save(); //делаем обычный сейв вместо "сохранить как"
+
+            ObjWorkExcel.Quit();
+            GC.Collect();
+            MessageBox.Show("Конец");*/
+
+            //-
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OPF = new OpenFileDialog();
+            if (OPF.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = OPF.FileName;
+                XPath = OPF.FileName;
+            }
+        }
+    }
+}
